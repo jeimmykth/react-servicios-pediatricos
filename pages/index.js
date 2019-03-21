193f49a660e00ffc6b3e8aee7a57bf131a875146
Jeimmy Garcia <jeimmykthgarcia@gmail.com>
2019-03-21 16:10:42 -0500

    imagen index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Index = (props) => (
   <Layout>
     <div>
       <h1>Bienvenidos a Servicios Pediatricos</h1>
+      <img src="http://bricammedical.com/wp-content/uploads/ADC/estetoscopio-pediatria-618-verde-adimals.jpg"></img>
       <p>Busca Servicios Disponibles</p>
     </div>
   </Layout>
